1ba7e2b40555169b123c1216873bd27e2c619b3d
zahid <zahidhasmani07@gmail.com>
2023-06-16 18:00:28 +0000

    instantnoodlep: Initial bringup for SparkOS * Make sure PRODUCT_USES_QCOM_HARDWARE is disabled, if is activated it will cause somes errors * Credits to LineageOS (https://github.com/LineageOS/android_device_oneplus_instantnoodlep)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_instantnoodlep.mk
+    $(LOCAL_DIR)/spark_instantnoodlep.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_instantnoodlep-user \
-    lineage_instantnoodlep-userdebug \
-    lineage_instantnoodlep-eng
+    spark_instantnoodlep-user \
+    spark_instantnoodlep-userdebug \
+    spark_instantnoodlep-eng
